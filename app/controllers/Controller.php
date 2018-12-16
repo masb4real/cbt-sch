d224a542d9e7ef395785c2f3395048283ee9683c
= <=>
2018-12-16 12:19:54 +0100

    Updated Controller file

--- app/controllers/Controller.php
+++ app/controllers/Controller.php
@@ -1,35 +1,35 @@
 <?php 
 class Controller {
 
-    protected $app;
-    protected $db;
+  protected $app;
+  protected $db;
 
-    function beforeroute() {
-    }
+  function beforeroute() {
+  }
 
-    function afterroute() {
-    }
+  function afterroute() {
+  }
 
-    function __construct() {
-        // this the F3 db connection instance
-        $app=Base::instance();
-        $db=new DB\SQL(
-            $app->get('db_dns') . $app->get('db_name'),
-            $app->get('db_user'),
-            $app->get('db_pass'),
-            array( \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION )
-        );
+  function __construct() {
+    // this the F3 db connection instance
+    $app=Base::instance();
+    $db=new DB\SQL(
+      $app->get('db_dns') . $app->get('db_name'),
+      $app->get('db_user'),
+      $app->get('db_pass'),
+      array( \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION )
+    );
 
-	$this->app=$app;
+    $this->app=$app;
     $this->db=$db;
-    }
-    
-    //this method is my custom connection instance for use where I desire more control
-    function customConn(){
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $dbname = "cbt";
-        return new mysqli($servername, $username, $password, $dbname); 
-    }
+  }
+  
+  //this method is my custom connection instance for use where I desire more control
+  function customConn(){
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "cbt";
+    return new mysqli($servername, $username, $password, $dbname); 
+  }
 }
